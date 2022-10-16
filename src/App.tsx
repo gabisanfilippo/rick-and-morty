69d241ffae212755c263ed599e38e1da5bdfcaa4
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2022-10-16 13:41:57 -0300

    feat: adding jotai provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider } from "react-router-dom";
 import { Router } from './routes/index';
 import React from "react";
 import { GlobalStyle } from "./styles";
+import { Provider as JotaiProvider } from "jotai";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle />
-      <RouterProvider router={Router} />
+      <JotaiProvider>
+        <GlobalStyle />
+        <RouterProvider router={Router} />
+      </JotaiProvider>
     </div>
   );
 }
